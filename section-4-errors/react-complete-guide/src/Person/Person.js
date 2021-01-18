6053651e5be020b1fc8a0833146db086a61bac09
Idris Khan <idris.khan303@gmail.com>
2021-01-18 17:14:41 +0000

    section 4

---
+++ section-4-errors/react-complete-guide/src/Person/Person.js
@@ -0,0 +1,18 @@
+const person = (props) => {
+  return (
+    <div className="Person">
+      <p onClick={props.click}>
+        My name is {props.name}, and I am {props.age} years old.
+      </p>
+      <p>{props.children}</p>
+      <input
+        className="Person__input"
+        type="text"
+        onChange={props.changed}
+        value={props.name}
+      />
+    </div>
+  );
+};
+
+export default person;
